b851779f210078579c1007a78d36fc6bfd0428bf
Matheus Santos Soares <mss.soares22@gmail.com>
2021-09-30 14:04:51 -0300

    feat: added real cartSize to header counter

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,25 +1,36 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
+import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
+import {StackNavigationProp} from '@react-navigation/stack';
 import {
   AbsoluteContainter,
   CardItemsQuantityContainer,
   CardItemsQuantityNumber,
-  Container,
 } from './styles';
+import {RootStackParamList} from '../../../App';
+import {useCart} from '../../hooks/useCart';
 
-export function Header() {
+type HeaderScreenProp = StackNavigationProp<RootStackParamList, 'Header'>;
+
+export function Header({}) {
+  const navigation = useNavigation<HeaderScreenProp>();
+  const {cart} = useCart();
+  const cartSize = cart.length;
   return (
     <>
-      <Icon
-        name="shoppingcart"
-        size={35}
-        color="#008e5a"
-        style={{marginRight: 20}}
-        /* onPress={() => navigation.navigate('Cart')} */
-      />
+      <TouchableOpacity>
+        <Icon
+          name="shoppingcart"
+          size={35}
+          color="#008e5a"
+          style={{marginRight: 20}}
+          onPress={() => navigation.navigate('Cart')}
+        />
+      </TouchableOpacity>
       <AbsoluteContainter>
         <CardItemsQuantityContainer>
-          <CardItemsQuantityNumber>1</CardItemsQuantityNumber>
+          <CardItemsQuantityNumber>{cartSize}</CardItemsQuantityNumber>
         </CardItemsQuantityContainer>
       </AbsoluteContainter>
     </>
