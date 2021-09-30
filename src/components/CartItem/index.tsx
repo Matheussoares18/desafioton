e90168b4804c9aca0fe9bcde051e179ebec1a1e4
Matheus Santos Soares <mss.soares22@gmail.com>
2021-09-30 14:07:58 -0300

    feat: cart screen now gets the products on cart from the cart Context

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -15,27 +15,34 @@ import {
 } from './styles';
 import Icon from 'react-native-vector-icons/AntDesign';
 import Tenis from '../../assets/tennis.png';
+import {Product, useCart} from '../../hooks/useCart';
 
-export function CartItem() {
+interface CardItemProps {
+  product: Product;
+  amount: number;
+}
+
+export function CartItem({product, amount}: CardItemProps) {
+  const {addProduct, decreaseQuantity, removeProduct} = useCart();
   return (
     <Container>
       <LeftSide>
-        <ItemImage source={Tenis} resizeMode="contain" />
+        <ItemImage source={{uri: product.image}} resizeMode="contain" />
         <TitleAndActions>
-          <ProductTitle>Tenis de marca</ProductTitle>
+          <ProductTitle>{product.name}</ProductTitle>
           <Actions>
-            <DecreaseQuantity>
+            <DecreaseQuantity onPress={() => decreaseQuantity(product.id)}>
               <ChangeQuantityText>-</ChangeQuantityText>
             </DecreaseQuantity>
-            <Counter>2</Counter>
-            <IncreaseQuantity>
+            <Counter>{amount}</Counter>
+            <IncreaseQuantity onPress={() => addProduct(product)}>
               <ChangeQuantityText>+</ChangeQuantityText>
             </IncreaseQuantity>
           </Actions>
         </TitleAndActions>
       </LeftSide>
       <RightSide>
-        <RemoveButton>
+        <RemoveButton onPress={() => removeProduct(product.id)}>
           <Icon name="close" color="red" size={20} />
         </RemoveButton>
       </RightSide>
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {CartItem} from '../../components/CartItem';
+import {useCart} from '../../hooks/useCart';
 import {Container, Content, PageHeader} from './styles';
 
+interface CartItemsAmount {
+  [key: number]: number;
+}
+
 export function Cart() {
+  const {cart} = useCart();
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = {...sumAmount};
+
+    newSumAmount[product.product.id] = product.quantity;
+
+    return newSumAmount;
+  }, {} as CartItemsAmount);
   return (
     <Container>
-      <PageHeader>2 produtos adicionados:</PageHeader>
+      <PageHeader>
+        {cart.length}{' '}
+        {cart.length > 1 ? 'produtos adicionados' : 'produto adicionado'}:
+      </PageHeader>
       <Content>
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {cart.map(cartItem => (
+          <CartItem
+            product={cartItem.product}
+            amount={cartItemsAmount[cartItem.product.id]}
+          />
+        ))}
       </Content>
     </Container>
   );
