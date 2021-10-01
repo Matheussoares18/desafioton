80a3ae019b7a0e8628bcdc14f3690b6f7504a168
Matheus Santos Soares <mss.soares22@gmail.com>
2021-10-01 13:03:01 -0300

    feat: added api mocks

--- src/screens/Home/index.spec.tsx
+++ src/screens/Home/index.spec.tsx
@@ -1,18 +1,61 @@
 import React from 'react';
-import {fireEvent, render} from '@testing-library/react-native';
+import {render, waitFor} from '@testing-library/react-native';
 import {Home} from '.';
-import {CartProvider, useCart} from '../../hooks/useCart';
-import {mockedNavigate} from '../../../jest/setup';
-import {renderHook} from '@testing-library/react-hooks';
+import {useCart} from '../../hooks/useCart';
+import AxiosMock from 'axios-mock-adapter';
+
+import api from '../../services/api';
 
 const mockedAddProduct = jest.fn();
 const mockedDecreaseProductQuantity = jest.fn();
 const mockedUseCartHook = useCart as jest.Mock;
 
+const apiMock = new AxiosMock(api);
 jest.mock('../../hooks/useCart');
-
 describe('Home', () => {
-  beforeEach(() => {
+  beforeAll(() => {
+    apiMock.onGet('products').reply(200, [
+      {
+        id: 1,
+        name: 'Tênis Adidas',
+        quantity: 10,
+        price: 220,
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+        image: 'https://www.imagemhost.com.br/images/2021/09/30/images-1.jpg',
+      },
+      {
+        id: 2,
+        name: 'Tênis Nike',
+        quantity: 2,
+        price: 250,
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+        image: 'https://www.imagemhost.com.br/images/2021/09/30/images.jpg',
+      },
+      {
+        id: 3,
+        name: 'Tênis Mizuno',
+        quantity: 4,
+        price: 180,
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+        image: 'https://www.imagemhost.com.br/images/2021/09/30/904105370.jpg',
+      },
+      {
+        id: 4,
+        name: 'Tênis Vans',
+        quantity: 3,
+        price: 200,
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+        image:
+          'https://www.imagemhost.com.br/images/2021/09/30/Tenis-Vans-Wm-Ward-0.jpg',
+      },
+    ]);
+  });
+
+  it('should render list without error', () => {
     mockedUseCartHook.mockReturnValue({
       cart: [
         {
@@ -67,9 +110,5 @@ describe('Home', () => {
       addProduct: mockedAddProduct,
       decreaseQuantity: mockedDecreaseProductQuantity,
     });
-  });
-
-  it('should render list without error', () => {
-    expect(render(<Home />)).toBeDefined();
   });
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {Container, ItemsContainer, ScrollViewContainer} from './styles';
 
 import {ProductCard} from '../../components/ProductCard';
 
-import {useCart} from '../../hooks/useCart';
+import {Product, useCart} from '../../hooks/useCart';
+
+import api from '../../services/api';
+import {Text} from 'react-native';
+import {AxiosResponse} from 'axios';
 
 interface CartItemsAmount {
   [key: number]: number;
 }
-
 export function Home() {
-  const {products, cart} = useCart();
+  const {cart} = useCart();
+  const [products, setProducts] = useState<Product[]>([]);
 
   const cartItemsAmount = cart.reduce((sumAmount, product) => {
     const newSumAmount = {...sumAmount};
@@ -21,11 +25,20 @@ export function Home() {
     return newSumAmount;
   }, {} as CartItemsAmount);
 
+  useEffect(() => {
+    async function loadProducts() {
+      api.get<any, AxiosResponse<Product[]>>('/products').then(res => {
+        setProducts(res.data);
+      });
+    }
+    loadProducts();
+  }, []);
+
   return (
     <Container>
       <ScrollViewContainer>
         <ItemsContainer testID="items-containter">
-          {products?.map(item => (
+          {products.map(item => (
             <ProductCard
               amount={cartItemsAmount[item.id]}
               product={item}
