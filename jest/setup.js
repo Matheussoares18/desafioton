6d68a7e1b6b592111814139c63649658c8914d32
Matheus Santos Soares <mss.soares22@gmail.com>
2021-10-01 13:01:44 -0300

    feat: added jest mocks

---
+++ jest/setup.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-vector-icons/AntDesign', () => 'Icon');
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+export const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
+jest.mock('@react-navigation/stack');
